7ad1739e8828e747dbc4303437b3c602ccbde79e
M-Chichava <76664047+M-Chichava@users.noreply.github.com>
2021-01-24 20:05:31 +0200

    cubo class

--- src/org/yourorghere/Transf_Geometricas.java
+++ src/org/yourorghere/Transf_Geometricas.java
@@ -139,7 +139,7 @@ public class Transf_Geometricas implements GLEventListener {
         // Desenho Terminado da Circunferência
         gl.glEnd();
         
-        
+       
 
         // Descarrega todas as operações de desenho na placa gráfica
         gl.glFlush();
