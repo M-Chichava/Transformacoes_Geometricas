9d67afc85ba7320388dd97c9306558e387e59a3c
M-Chichava <76664047+M-Chichava@users.noreply.github.com>
2021-01-24 22:03:30 +0200

    Update Cilindro.java

--- src/org/yourorghere/Cilindro.java
+++ src/org/yourorghere/Cilindro.java
@@ -13,6 +13,7 @@ import com.sun.j3d.utils.applet.MainFrame;
 import com.sun.j3d.utils.universe.*;
 import javax.media.j3d.*;
 import javax.vecmath.*;
+import java.lang.UnsatisfiedLinkError;
 
 /**
  *
@@ -34,39 +35,38 @@ public class Cilindro extends Applet{
         Cylinder CylinderObj = new Cylinder();
         objScale.addChild(CylinderObj);
         BoundingSphere bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0), 100.0);
-// luzes
-Color3f lColor1 = new Color3f(1.0f, 0.0f, 0.0f);
-Color3f lColor2 = new Color3f(1.0f, 0.0f, 0.0f);
-Vector3f lDir1 = new Vector3f(-1.0f, -1.0f, -1.0f);
-Vector3f lDir2 = new Vector3f(0.0f, 0.0f, -1.0f);
-DirectionalLight lgt1 = new DirectionalLight(lColor1, lDir1);
-DirectionalLight lgt2 = new DirectionalLight(lColor2, lDir2);
-lgt1.setInfluencingBounds(bounds);
-lgt2.setInfluencingBounds(bounds);
-objScale.addChild(lgt1);
-objScale.addChild(lgt2);
-// otimiza conversao do grafo de cena para renderizacao
-// pela OpenGL
-objRoot.compile();
-return objRoot;
-}
-public Cilindro (){
-setLayout(new BorderLayout());
-GraphicsConfiguration config =
-SimpleUniverse.getPreferredConfiguration();
-Canvas3D c = new Canvas3D(config);
-add("Center", c);
-// Cria um grafo simple e o anexa ao virtual universe
-BranchGroup scene = createSceneGraph();
-SimpleUniverse u = new SimpleUniverse(c);
-// Move ViewPlatform para tras, para que
-// os objetos da cena sejam vistos
-u.getViewingPlatform().setNominalViewingTransform();
-u.addBranchGraph(scene);
-}
-public static void main(String argv[])
-{
-BranchGroup group;
-new MainFrame(new Cilindro(), 400, 400);
-}
+        // luzes
+        Color3f lColor1 = new Color3f(1.0f, 0.0f, 0.0f);
+        Color3f lColor2 = new Color3f(1.0f, 0.0f, 0.0f);
+        Vector3f lDir1 = new Vector3f(-1.0f, -1.0f, -1.0f);
+        Vector3f lDir2 = new Vector3f(0.0f, 0.0f, -1.0f);
+        DirectionalLight lgt1 = new DirectionalLight(lColor1, lDir1);
+        DirectionalLight lgt2 = new DirectionalLight(lColor2, lDir2);
+        lgt1.setInfluencingBounds(bounds);
+        lgt2.setInfluencingBounds(bounds);
+        objScale.addChild(lgt1);
+        objScale.addChild(lgt2);
+        // otimiza conversao do grafo de cena para renderizacao
+        // pela OpenGL
+        objRoot.compile();
+        return objRoot;
+    }
+    public Cilindro (){
+        setLayout(new BorderLayout());
+        GraphicsConfiguration config =
+        SimpleUniverse.getPreferredConfiguration();
+        Canvas3D c = new Canvas3D(config);
+        add("Center", c);
+        // Cria um grafo simple e o anexa ao virtual universe
+        BranchGroup scene = createSceneGraph();
+        SimpleUniverse u = new SimpleUniverse(c);
+        // Move ViewPlatform para tras, para que
+        // os objetos da cena sejam vistos
+        u.getViewingPlatform().setNominalViewingTransform();
+        u.addBranchGraph(scene);
+    }
+    public static void main(String argv[]){
+        new MainFrame(new Cilindro(), 400, 400);
+
+    }
 }
